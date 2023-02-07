73bb0be0610f208bdb4a7d5e782bc8c7b6187981
Ivan <dfyzcfvjqkjd10@gmail.com>
2023-02-07 13:54:20 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./models/product";
+import {ProductsService} from "./services/products.service";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-ex';
+export class AppComponent implements OnInit {
+  title = 'angular app'
+  // products: IProduct[] = []
+  products$: Observable<IProduct[]>
+  loading = false
+  constructor(private productsService: ProductsService) {
+  }
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(() => this.loading = false)
+    )
+    // this.productsService.getAll().subscribe(products => {
+    //   this.products = products
+    //   this.loading = false
+    // })
+  }
+
+
 }
